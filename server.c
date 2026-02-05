383859a6e325668579f9df6399add601d6b5a07d
yunie <syafieqayuni@gmail.com>
2026-02-05 20:49:39 +0800

    some error

--- server.c
+++ server.c
@@ -112,14 +112,14 @@ int isCorrect(GameState *game, int current) {
 }
 
 //----------- 5. YUNI -------------------------
-int wordIsComplete(GameState *game, int current_round, int total_rounds) {
+int wordIsComplete(GameState *game) {
     for (int i = 0; i < strlen(game->answer_word); i++) {
         if (game->answer_space[i] == '_')
             return 0; // word not complete yet
     }
 }
 //word is complete & end the game
-if (current_round == total_rounds) {
+if (game-> round == 5) {
 game->game_over = 1;
 } else {
     return 1; // continue to next round
